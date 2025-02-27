c738aec00d90c8e2b27c42f2c90f9b11af847c1c
Carlos Mondo <carlosmondo@fastmail.com>
2025-02-26 21:12:11 -0300

    ✨ Rust Hello World

---
+++ rust/hello_world/main.rs
@@ -0,0 +1,9 @@
+// fn - Define a function in Rust
+// main - It's the name of the function, in this case 'main' is the entry point of the program
+// () - It's the parameter list, in this case it's empty
+// {} - It's the body of the function
+// println! - It's a macro (macro is defined by the ! in the end of function name) that prints a string to the console
+// "Hello, world!" - It's the string that will be printed to the console
+fn main() {
+  println!("Hello, world!");
+}
